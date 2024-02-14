7cc7ba45021880f11e60f1adb22ee6bf33257f96
kelvinbe <kelvin.beno.k@gmail.com>
2024-02-14 17:07:13 +0300

    fetched residentds data

--- src/components/atoms/cards/Card.tsx
+++ src/components/atoms/cards/Card.tsx
@@ -1,6 +1,7 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Card.css'
+import { copyFileSync } from 'fs';
 
 
 interface ICard {
@@ -13,27 +14,45 @@ interface ICard {
   }
 
 const Card = (props: ICard) => {
-    const [resi, setRes] = useState(null)
+    const [resi, setRes] = useState([])
 
     const {image, name, type, residents} = props
 
-    const getResident = async (resident) => {
-    const res = await fetch(resident)
-    return res.json()
-    }
+    
+
+    useEffect(() => {
+      const getResident = async (residents) => {
+        const residentData = [];
+        for (let i = 0; i < residents.length; i++) {
+            const res = await fetch(residents[i]);
+            const data = await res.json();
+            residentData.push(data);
+        }
+        setRes(residentData);
+    };
+      
+        getResident(residents)
+
+    }, [residents])
+
+
+    console.log(resi.slice(0, 8))
 
   return (
 <div
   className="card-container">
   <div className="card-image-container">
-   {residents.slice(1, 2).map((resident) => {
-        
-            return <img
-            className="rounded-t-lg"
-            src={resi?.image}
-            alt="" />
-
-   })} 
+  {resi.slice(0, 3).map((resident, index) => {
+              return     <img
+                        key={resident}
+                        className="rounded-t-lg"
+                        src={resident.image}
+                        width={90}
+                        height={30}
+                        key={index}
+                        alt=""
+                    />
+  })}
   </div>
   <div className="flex flex-col px-3">
     <span>location:
